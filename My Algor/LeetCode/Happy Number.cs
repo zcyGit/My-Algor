058187f74174be7e6d1ee5be2ffd2b7f766207c0
zcy <304907240@qq.com>
2015-09-12 18:45:48 +0800

    添加

---
+++ My Algor/LeetCode/Happy Number.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+namespace My_Algor.LeetCode
+{
+    class Happy_Number
+    {
+
+        public static void Test()
+        {
+
+            int number = 7;
+            Console.WriteLine(IsHappy(number));
+        }
+
+
+
+        public static bool IsHappy(int number)
+        {
+            int ends = number;
+
+            while (ends != 1)
+            {
+
+                var numberArray = ends.ToString().ToArray();
+                ends = 0;
+                foreach (var num in numberArray)
+                {
+                    var numInt = Convert.ToInt32(num) - 48;
+                    ends += numInt * numInt;
+                }
+                if (ends == 1)
+                {
+                    return true;
+                }
+                if (ends <= 9)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+
+    }
+}
